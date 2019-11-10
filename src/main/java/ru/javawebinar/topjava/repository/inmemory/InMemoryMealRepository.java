80426909a11a6086adc747a382154f069e48e726
Nikolay Zhadovskii <nick9.bk@gmail.com>
2019-11-10 17:28:56 +0300

    Edit MealRepo with sign in user id

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,42 +5,61 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.util.UserUtil.SIGN_USER;
+import static ru.javawebinar.topjava.util.UserUtil.getSignUser;
 
 public class InMemoryMealRepository implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(meal -> this.save(getSignUser(), meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        Map<Integer, Meal> mealMap = repository.computeIfAbsent(userId, k -> new HashMap<>());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            mealMap.put(meal.getId(), meal);
             return meal;
         }
         // treat case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+
+        return mealMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int id) {
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        if (mealMap == null) {
+            return false;
+        }
+        return mealMap.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        return mealMap.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> mealMap = repository.get(userId);
+        return mealMap.values().stream().sorted(new Comparator<Meal>() {
+            @Override
+            public int compare(Meal o1, Meal o2) {
+                return o2.getDateTime().compareTo(o1.getDateTime());
+            }
+        }).collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/util/UserUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserUtil.java
@@ -13,9 +13,15 @@ public class UserUtil {
 
     public static final List<User> USERS;
 
+    private static final int SIGN_USER = 1;
+
+    public static int getSignUser() {
+        return SIGN_USER;
+    }
+
     static {
-        int i = 1;
-        USERS = Arrays.asList(new User(i++, "Nick" + i, "nick" + i + "@mail.ru", "1", Role.ROLE_ADMIN),
+        int i = 0;
+        USERS = Arrays.asList(new User(++i, "Nick" + i, "nick" + i + "@mail.ru", "1", Role.ROLE_ADMIN),
                 new User(i++, "Nick" + i, "nick" + i + "@mail.ru", "1", Role.ROLE_USER),
                 new User(i++, "Nick" + i, "nick" + i + "@mail.ru", "1", Role.ROLE_USER),
                 new User(i++, "Nick" + i, "nick" + i + "@mail.ru", "1", Role.ROLE_USER),
