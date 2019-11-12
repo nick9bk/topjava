66bffe749fb8a26b6b38196c39d99c15c9567106
Nikolay Zhadovsky <nick9.bk@gmail.com>
2019-11-12 10:22:01 +0300

    add meal table and populate

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meal;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -22,4 +23,17 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE public.meal
+(
+    id          integer PRIMARY KEY DEFAULT nextval('global_seq'),
+    calories    integer,
+    description text,
+    date_time   timestamp without time zone,
+    user_id     integer NOT NULL,
+    CONSTRAINT meal_id_fkey FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
