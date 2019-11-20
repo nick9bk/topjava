d829bd7aa4c133fe8d11a042acc09a3723bfc4a5
Nikolay Zhadovsky <nick9.bk@gmail.com>
2019-11-20 12:12:11 +0300

    Add HSQL JdbcMealRepo

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcHsqlMealRepository.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+
+import static ru.javawebinar.topjava.Profiles.HSQL_DB;
+import static ru.javawebinar.topjava.util.DateTimeUtil.getEndExclusive;
+import static ru.javawebinar.topjava.util.DateTimeUtil.getStartInclusive;
+
+@Repository
+@Profile(HSQL_DB)
+public class JdbcHsqlMealRepository extends JdbcMealRepositoryAbstract {
+
+    @Autowired
+    public JdbcHsqlMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=? AND date_time >=? AND date_time < ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, Timestamp.valueOf(getStartInclusive(startDate)), Timestamp.valueOf(getEndExclusive(endDate)));
+    }
+
+    @Override
+    protected MapSqlParameterSource getParameterSource(Meal meal, int userId) {
+        return new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("date_time", Timestamp.valueOf(meal.getDateTime()))
+                .addValue("user_id", userId);
+    }
+}
