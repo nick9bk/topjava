33d5f8324d65434ae3c56e1083d4e15815ee2e91
Nikolay Zhadovsky <nick9.bk@gmail.com>
2019-12-02 14:52:40 +0300

    Merge with origin repository

--- src/main/java/ru/javawebinar/topjava/web/json/JsonUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/json/JsonUtil.java
@@ -34,13 +34,4 @@ public class JsonUtil {
             throw new IllegalStateException("Invalid write to JSON:\n'" + obj + "'", e);
         }
     }
-
-    public static <T> String writeIgnoreProps(T obj, String... ignoreProps) {
-        Map<String, Object> map = getMapper().convertValue(obj, new TypeReference<Map<String, Object>>() {
-        });
-        for (String prop : ignoreProps) {
-            map.remove(prop);
-        }
-        return writeValue(map);
-    }
 }
